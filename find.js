52b8ca942febf4b82c7f950c81703e8c392de505
Ruben de la Fuente <rudelafuente@gmail.com>
2019-02-15 07:25:06 +0100

    Find

---
+++ find.js
@@ -0,0 +1,16 @@
+var mongo = require('mongodb').MongoClient
+var age = process.argv[2]
+
+var url = 'mongodb://localhost:27017/learnyoumongo'
+
+mongo.connect(url, function(err, db) {
+    var parrots = db.collection('parrots')
+  parrots.find({
+    age: {
+      $gt: +age
+    }
+  }).toArray(function(err, docs) {
+    console.log(docs)
+    db.close()
+  })
+},{ useNewUrlParser: true })
