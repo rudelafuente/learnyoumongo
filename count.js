fded49af828add87a536cb05105d8d3e8270a1e7
Ruben de la Fuente <rudelafuente@gmail.com>
2019-02-16 07:43:24 +0100

    Count

---
+++ count.js
@@ -0,0 +1,13 @@
+
+
+var mongo = require('mongodb').MongoClient
+mongo.connect('mongodb://localhost:27017', function(err, client) {
+  const db = client.db('learnyoumongo');
+  db.collection('parrots').find({
+    age : {
+      $gt: Number(process.argv[2])
+  }}).count(function(err, data) {
+    console.log(data)
+    client.close();
+  });
+});
